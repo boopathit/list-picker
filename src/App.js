32adeb7688727d9bb00dd2c4cc33b9bd1f6d8463
Boopathi T <getboopathi.t@gmail.com>
2022-02-23 09:00:09 +0530

    add onchage listener, api payload skeleton

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Container from "react-bootstrap/Container";
 import ListPicker from "./ListPicker";
 import "./App.css";
 
 const App = () => {
-  const leftRows = [
+  const listOfRows = [
     { id: "1", name: "1" },
     { id: "12", name: "12" },
     { id: "13", name: "13" },
@@ -13,21 +13,33 @@ const App = () => {
     { id: "15", name: "15" },
     { id: "16", name: "16" },
     { id: "17", name: "17" },
-    { id: "18", name: "18" }
+    { id: "18", name: "18" },
   ];
-  const rightRows = [
-    { id: "1", name: "1" },
-    { id: "2", name: "2" },
-    { id: "3", name: "3" },
-    { id: "4", name: "4" },
-    { id: "5", name: "5" },
-    { id: "6", name: "6" },
-    { id: "7", name: "7" },
-    { id: "8", name: "8" }
-  ];
+  const selectedListOfRows = [];
+
+  const [availableList, setAvailableList] = useState([]);
+  const [selectedList, setSelectedList] = useState([]);
+
+  const onListChange = (leftRows, rightRows) => {
+    setAvailableList(leftRows);
+    setSelectedList(rightRows);
+  };
+
+  const onSave = () => {
+    var data = {
+      availableList,
+      selectedList,
+    };
+    console.log("Save to API", JSON.stringify(data));
+  };
   return (
     <Container fluid>
-      <ListPicker leftRows={leftRows} rightRows={rightRows} />
+      <ListPicker
+        leftRows={listOfRows}
+        rightRows={selectedListOfRows}
+        onChange={onListChange}
+      />
+      <button onClick={onSave}>Save</button>
     </Container>
   );
 };
